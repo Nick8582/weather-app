427f6aba70342c3f3afb09c0751695fafbeb5ded
Vladislav Potapov <89172111013q@gmail.com>
2023-07-30 19:56:16 +0300

    add: content per country of Documentation'

--- src/pages/DocumentationPage.vue
+++ src/pages/DocumentationPage.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    Documentation
+    <p class="documentation__text">This project was created with the aim of mastering the practice and developing skills with the work of the API.</p>
+    <p class="documentation__text">To work with this project, you need to go to the <router-link to="/">"Home"</router-link> page and enter the required city. Then click on the "add" button.</p>
+    <p class="documentation__text">The project was created by me. You can go to my <a href="https://github.com/Nick8582">GitHub page</a>.</p>
   </div>
 </template>
 
